ead5f472783ba37cd5a84327b45dcfb6378a1ef0
Salah <salahgabana@gmail.com>
2023-10-06 17:04:16 +0200

    Primer hola mundo

---
+++ poo0/src/main/java/com/mycompany/poo0/Poo0.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo0;
+
+/**
+ *
+ * @author manana
+ */
+public class Poo0 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
